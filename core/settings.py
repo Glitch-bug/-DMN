15a4f2a13c96b5a601f0dd415d4b7b42b5e4add8
KobbyCas <127396214+KobbyCas@users.noreply.github.com>
2023-04-09 13:16:52 +0000

    CSRF trusted

--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+#CSRF
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-b2f4.up.railway.app/'
+]
